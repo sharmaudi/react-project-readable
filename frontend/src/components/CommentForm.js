532f8462a66e4f1f05f4b46dc4a0d003c6cd90b1
Udit Sharma <sharma.uditt@gmail.com>
2017-09-21 14:56:27 +1000

    Comment form shows default message if no comments are provided.

--- frontend/src/components/CommentForm.js
+++ frontend/src/components/CommentForm.js
@@ -6,17 +6,14 @@ class CommentForm extends Component {
             <div className="well">
                 <h4>Leave a comment</h4>
                 <form className="clearfix">
-                    <div className="col-md-6 form-group">
+                    <div className="col-md-12 form-group">
                         <label className="sr-only" htmlFor="name">Name</label>
                         <input type="text" className="form-control" id="name" placeholder="Name"/>
                     </div>
-                    <div className="col-md-6 form-group">
-                        <label className="sr-only" htmlFor="email">Email</label>
-                        <input type="email" className="form-control" id="email" placeholder="Email"/>
-                    </div>
+
                     <div className="col-md-12 form-group">
                         <label className="sr-only" htmlFor="email">Comment</label>
-                        <textarea className="form-control" id="comment" placeholder="Comment"/>
+                        <textarea rows="6" className="form-control" id="comment" placeholder="Comment"/>
                     </div>
                     <div className="col-md-12 form-group text-right">
                         <button type="submit" className="btn btn-primary">Submit</button>
--- frontend/src/components/Comments.js
+++ frontend/src/components/Comments.js
@@ -6,8 +6,11 @@ class Comments extends Component {
 
     showComments() {
         const comments = this.props.comments
-        return comments.map(comment => (
-            <li className="comment" key={comment.id}>
+
+        if (comments.length > 0) {
+
+            return comments.map(comment => (
+                <li className="comment" key={comment.id}>
                     <div className="clearfix">
                         <h4 className="pull-left">{comment.author}</h4>
                         <p className="pull-right">{formatDate(comment.timestamp)}</p>
@@ -15,24 +18,41 @@ class Comments extends Component {
                     <p>
                         <em>{comment.body}</em>
                     </p>
-            </li>
-        ))
+                </li>
+            ))
+        } else {
+            return (
+                <li className="comment" key="1">
+                    <p>
+                        <em>No Comments yet</em>
+                    </p>
+                </li>
+            )
+        }
+
+
     }
 
-    render() {
-        return (
-            
-            
-            <div className="well"><ul id="comments" className="comments">
+render()
+{
+    return (
+
+
+
+        <div className="well">
+            <ul id="comments" className="comments">
                 {this.showComments()}
-            </ul></div>
-            
-        );
-    }
+            </ul>
+        </div>
+
+
+
+    );
+}
 }
 
 Comments.propTypes = {
-    comments:PropTypes.object.isRequired
+    comments: PropTypes.object.isRequired
 };
 Comments.defaultProps = {};
 
